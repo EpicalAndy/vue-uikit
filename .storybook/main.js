adbf1a82ff1fa958453b82154a032a51d2da1425
Andy <lun9nin@inbox.ru>
2023-09-26 17:01:09 +0300

    Init project

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  disableTelemetry: false,
+};
+export default config;
